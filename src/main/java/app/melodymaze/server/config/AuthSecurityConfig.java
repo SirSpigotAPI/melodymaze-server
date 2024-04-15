1c7fad91d10e91a16cef6658a446cab75fd6f329
SirSpigotAPI <tonidenk1@gmail.com>
2024-04-15 22:02:08 +0200

    add spring oauth2 client

--- src/main/java/app/melodymaze/server/config/AuthSecurityConfig.java
+++ src/main/java/app/melodymaze/server/config/AuthSecurityConfig.java
@@ -46,7 +46,7 @@ public class AuthSecurityConfig {
 
         http.exceptionHandling((exceptions) -> exceptions
                         .defaultAuthenticationEntryPointFor(
-                                new LoginUrlAuthenticationEntryPoint("/login"),
+                                new LoginUrlAuthenticationEntryPoint("/login/oauth2"),
                                 new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
                         )
         ).oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
--- src/main/java/app/melodymaze/server/controller/Oauth2LoginController.java
+++ src/main/java/app/melodymaze/server/controller/Oauth2LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Oauth2LoginController {
 
-    @RequestMapping("/login")
+    @RequestMapping("/login/oauth2")
     public String loginPage() {
         return "login";
     }
