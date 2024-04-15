ad797bb6fcb324947974070572b2e7dc979d72be
SirSpigotAPI <tonidenk1@gmail.com>
2024-04-15 18:12:41 +0200

    Initial commit

---
+++ src/main/java/app/melodymaze/server/MelodymazeServerApplication.java
@@ -0,0 +1,13 @@
+package app.melodymaze.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelodymazeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MelodymazeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/melodymaze/server/MelodymazeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.melodymaze.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelodymazeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
