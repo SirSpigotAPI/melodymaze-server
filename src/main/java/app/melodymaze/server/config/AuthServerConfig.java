bc626e626eda8936003f5545e6379a696297dd4a
SirSpigotAPI <tonidenk1@gmail.com>
2024-04-16 21:28:52 +0200

    Add cors configuration; Refactor AuthSecurityConfig; add application-local.yml

---
+++ src/main/java/app/melodymaze/server/config/AuthServerConfig.java
@@ -0,0 +1,15 @@
+package app.melodymaze.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+
+@Configuration
+public class AuthServerConfig {
+
+    @Bean
+    public AuthorizationServerSettings authorizationServerSettings() {
+        return AuthorizationServerSettings.builder().build();
+    }
+
+}
