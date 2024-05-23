35ba01645133c3c8e168b2646cb501391a6f3220
SirSpigotAPI <tonidenk1@gmail.com>
2024-05-23 23:45:32 +0200

    feature: implement logout

--- src/main/java/app/melodymaze/server/config/AuthSecurityConfig.java
+++ src/main/java/app/melodymaze/server/config/AuthSecurityConfig.java
@@ -1,8 +1,6 @@
 package app.melodymaze.server.config;
 
-import app.melodymaze.server.handler.LogoutErrorHandler;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.util.http.parser.HttpParser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -10,7 +8,6 @@ import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
