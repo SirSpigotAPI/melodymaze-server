85c43050cd502edfcf75277e7697c2a9ab53ffbf
SirSpigotAPI <tonidenk1@gmail.com>
2024-06-01 23:46:50 +0200

    feature: make issuer url configurable

--- src/main/java/app/melodymaze/server/config/AuthServerConfig.java
+++ src/main/java/app/melodymaze/server/config/AuthServerConfig.java
@@ -1,5 +1,6 @@
 package app.melodymaze.server.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
@@ -7,9 +8,12 @@ import org.springframework.security.oauth2.server.authorization.settings.Authori
 @Configuration
 public class AuthServerConfig {
 
+    @Value("${melodymaze.authorization.server.issuer-url}")
+    private static String issuerUrl;
+
     @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
-        return AuthorizationServerSettings.builder().issuer("http://localhost:9001").build();
+        return AuthorizationServerSettings.builder().issuer(issuerUrl).build();
     }
 
 }
